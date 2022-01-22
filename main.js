8d93ab0cbd64a8e96c859dbc46d6a6c0a308d620
Mylene <pont.mylene@gmail.com>
2022-01-22 13:14:23 +0900

    L9 done components and props

--- main.js
+++ main.js
@@ -2,28 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            url: "https://www.nordstrom.com/browse/men/clothing/socks?filterByColor=green",
-            inventory: 10,
-            onSale: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
-            ],
-            sizes: ["S", "M", "L"]
+            premium: false,
         }
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        removeFromCart() {
-            this.cart -=1
-        },
-    updateImage(variantImage) {
-        this.image = variantImage
-        }
-    }
+    methods: {}    
 })
+
