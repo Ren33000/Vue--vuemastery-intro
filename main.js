6d02ff194b9197f03e9a6afd5415586161d75ba8
Mylene <pont.mylene@gmail.com>
2022-01-22 11:25:54 +0900

    L4 done, if/else show

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            url: "https://www.nordstrom.com/browse/men/clothing/socks?filterByColor=green"
+            url: "https://www.nordstrom.com/browse/men/clothing/socks?filterByColor=green",
+            inventory: 100,
+            onSale: true,
         }
     }
 })
